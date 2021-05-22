9a544b755f9876ff81f887a2cee2e023c9245567
Parth Rewoo <67362433+Parth1906@users.noreply.github.com>
2021-05-22 12:51:57 +0530

    Update and rename GroupB_Practical3.cpp to GroupB_Practical7.cpp

--- GroupB_Practical7.cpp
+++ GroupB_Practical7.cpp
@@ -1,5 +1,5 @@
 /*
-Experiment 3 : Construct an expression tree from the given prefix and traverse it using post order traversal and then delete the entire tree.
+Experiment 7 : Construct an expression tree from the given prefix and traverse it using post order traversal and then delete the entire tree.
 */
 #include <iostream>
 #include <string.h>

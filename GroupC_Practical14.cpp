672d715833d8e5572f7d1a5d83588edeb181a90e
Parth Rewoo <67362433+Parth1906@users.noreply.github.com>
2021-05-22 13:08:02 +0530

    Update GroupC_Practical14.cpp

--- GroupC_Practical14.cpp
+++ GroupC_Practical14.cpp
@@ -1,5 +1,7 @@
 /*
-
+Experiment 14 : There are flight paths between cities. If there is a flight between City A and City B then there is an edge between the cities. 
+The cost of the edge can be the time that flight take to reach city B from A, or the amount of fuel used for the journey. Represent this as a graph.
+The node can be represented by the airport name or name of the city. Use adjacency list representation of the graph or use adjacency matrix representation of the graph.
 */
 
 #include <iostream>
